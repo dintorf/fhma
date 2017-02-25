72a93cf6f2d57e34115f32e776b1e943d62804c8
dintorf <dintorf@gmail.com>
2017-02-25 12:57:04 -0700

    Created warrior camp countdown and added some info

---
+++ fountainhillsmartialarts.com/_/components/php/nextEvent.php
@@ -0,0 +1,12 @@
+<div id="about" class="col-lg-8 col-lg-push-2 col-md-8 col-md-push-2 col-sm-8 col-sm-push-2 center">
+	<div class="leftSection col-lg-6 col-md-6 col-sm-6">
+		<h2>Get Ready for Warrior Camp!</h2><br />
+		<p>Grab your tactical pants, training gear, and eye protection because Warrior Camp is just around the corner! Sign your child up now for an experience that they will never forget. Get ready for a weekend of intense training, laughs, and memories!</p>
+	</div>
+	<div class="rightSection col-lg-6 col-lg-push-1 col-md-6 col-sm-6">
+		<h1 class="text-center">July 20 to July 23</h1>
+		<h2 class="text-center">Camp Shadow Pines</h2>
+		<h2 class="text-center">Heber, AZ</h2>
+		<h3 class="text-center">Call (602) 499-3778 to reserve your spot!</h3>
+	</div>
+</div>
